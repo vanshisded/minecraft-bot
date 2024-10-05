811eb2ea9041883badc022b3e79c4a8bf9bac3b4
vanshisded <buisness.vansh@gmail.com>
2024-10-05 12:29:00 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,24 +1,32 @@
 const mineflayer = require('mineflayer');
 
-const bot = mineflayer.createBot({
-  host: 'vanshENBF.aternos.me', // Replace with your Minecraft server IP
-  port: 29303,            // The port number (default is 25565)
-  username: 'BOTv2', // The username for the bot
-  version: false          // Or specify the version number
-});
+// Function to start the bot
+function startBot() {
+  const bot = mineflayer.createBot({
+    host: 'vanshENBF.aternos.me', // Replace with your Minecraft server IP
+    port: 29303,                  // The port number (default is 25565)
+    username: 'BOTv2',           // The username for the bot
+    version: false                // Or specify the version number
+  });
 
-bot.on('spawn', () => {
-  console.log('Bot has spawned and is connected to the server');
-});
+  // Event: Bot has spawned and is connected to the server
+  bot.on('spawn', () => {
+    console.log('Bot has spawned and is connected to the server');
+  });
 
-bot.on('error', (err) => {
-  console.log('Error:', err);
-});
+  // Event: Error occurred
+  bot.on('error', (err) => {
+    console.log('Error:', err);
+  });
 
-bot.on('end', () => {
-  console.log('Bot has disconnected. Reconnecting...');
-  // Automatically reconnect
-  setTimeout(() => {
-    bot.connect();
-  }, 5000);
-});
+  // Event: Bot has disconnected, attempting to reconnect
+  bot.on('end', () => {
+    console.log('Bot has disconnected. Reconnecting...');
+    setTimeout(() => {
+      startBot(); // Restart the bot
+    }, 5000);
+  });
+}
+
+// Export the startBot function
+module.exports = { startBot };
