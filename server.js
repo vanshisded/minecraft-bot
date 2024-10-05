8e65c4757ad3fe1a7e4a6b2860f511ba4504b406
vanshisded <buisness.vansh@gmail.com>
2024-10-05 12:26:02 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 // Import the required modules
 const express = require('express');
+const { startBot } = require('./bot'); // Import your bot function from bot.js
 
 // Create an instance of an Express application
 const app = express();
@@ -9,8 +10,11 @@ const PORT = process.env.PORT || 3000;
 
 // Set up a basic route
 app.get('/', (req, res) => {
-  res.send('Hello, world! Your server is running!');
+  res.send('Hello, world! Your server is running and the bot is connected!');
 });
+
+// Start the bot
+startBot(); // Call the function that starts the bot
 
 // Start the server and listen on the specified port
 app.listen(PORT, () => {
